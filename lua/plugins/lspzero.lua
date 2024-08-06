8690c3656c0ac5b6c1ad6a0071d294237835b6ae
justindudududu29 <justinduchengkai@gmail.com>
2024-08-06 16:49:38 +0800

    update lsp_zero to v4

--- lua/plugins/lspzero.lua
+++ lua/plugins/lspzero.lua
@@ -1,11 +1,10 @@
 --- Uncomment the two plugins below if you want to manage the language servers from neovim
-return{
-{'williamboman/mason.nvim'},
-{'williamboman/mason-lspconfig.nvim'},
+return {
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
 
-{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-{'neovim/nvim-lspconfig'},
-{'hrsh7th/cmp-nvim-lsp'},
-{'hrsh7th/nvim-cmp'},
-{'L3MON4D3/LuaSnip'},
+    { 'VonHeikemen/lsp-zero.nvim',        branch = 'v4.x' },
+    { 'neovim/nvim-lspconfig' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/nvim-cmp' },
 }
